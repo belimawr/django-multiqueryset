fe79f4791436d97a487e093174b1baeb90eb4fd4
Tiago Queiroz <contato@tiago.eti.br>
2015-01-21 16:23:50 -0200

    Renaming stuff, TODO list

---
+++ README.rst
@@ -0,0 +1,10 @@
+====================
+django-multiqueryset
+====================
+A class to wrapper Django QuerySets and lists together providing slice and access to specific elements
+
+====
+TODO
+====
+ - Better documentation and examples
+ - Code improvements
--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     version='0.9',
     author='Tiago Queiroz',
     author_email='contato@tiago.eti.br',
-    packages=['django-multiqueryset'],
+    packages=['django_multiqueryset'],
     url='http://github.com/belimawr/django-multiqueryset.git',
     license='LGPLv2.1',
     description=' class to wrapper Django QuerySets and lists together providing slice and access to specific elements',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
+    install_requires=[],
 )
